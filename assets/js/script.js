ba1b095dd7d30020a4e0365a9541cf44b4750b74
Grupo4 <143132283+grupo-4-ucv@users.noreply.github.com>
2024-07-10 05:50:22 -0500

    Version 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,20 @@ function openNav(){
 function closeNav(){
     document.getElementById("mobile-menu").style.width = "0%";
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const text = document.querySelector('.texto-1');
+    const images = document.querySelectorAll('.imagen');
+
+    // Añadir clase 'visible' con retraso para el texto
+    setTimeout(() => {
+        text.classList.add('visible');
+    }, 500); // Retraso de 0.5 segundos
+
+    // Añadir clase 'visible' con retraso para cada imagen
+    images.forEach((image, index) => {
+        setTimeout(() => {
+            image.classList.add('visible');
+        }, (index + 1) * 500); // Retraso de 0.5 segundos por imagen
+    });
+});
